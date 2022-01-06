0d795e42dd1c30d8997e5de8131e0040cf2b4629
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-01-05 23:20:16 -0500

    task 01/05/2022 done

---
+++ src/main/java/JavaTask01052022.java
@@ -0,0 +1,36 @@
+public class JavaTask01052022 {
+
+    //https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/java
+
+    public static String rps(String p1, String p2) {
+
+        String result = "";
+        if (p1.equals("scissors")) {
+            if (p2.equals("paper")) {
+                result = "Player 1 won!";
+            } else if (p2.equals("rock")) {
+                result = "Player 2 won!";
+            } else {
+                result = "Draw!";
+            }
+        } else if (p1.equals("paper")) {
+            if (p2.equals("rock")) {
+                result = "Player 1 won!";
+            } else if (p2.equals("scissors")) {
+                result = "Player 2 won!";
+            } else {
+                result = "Draw!";
+            }
+        } else if (p1.equals("rock")) {
+            if (p2.equals("scissors")) {
+                result = "Player 1 won!";
+            } else if (p2.equals("paper")) {
+                result = "Player 2 won!";
+            } else {
+                result = "Draw!";
+            }
+        }
+
+        return result;
+    }
+}
