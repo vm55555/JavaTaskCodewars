59a00fc806329db42d3389c92978fc30315cbe3b
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-01-06 21:04:10 -0500

    task 01/06/2022 done

---
+++ src/main/java/JavaTask01062022.java
@@ -0,0 +1,29 @@
+public class JavaTask01062022 {
+
+    //https://www.codewars.com/kata/57f222ce69e09c3630000212/train/java
+
+    public static String well(String[] x) {
+        String result = "";
+        int count = 0;
+
+        for (String s : x) {
+            if (s.equals("good")) {
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            result = "Fail!";
+        } else if (count > 0 && count < 3) {
+            result = "Publish!";
+        } else {
+            result = "I smell a series!";
+        }
+
+        return result;
+    }
+
+    public static void main (String [] args) {
+        System.out.println(well(new String[] {"bad", "bad", "bad"}));
+    }
+}
