67c8fe84fafc49049aa111d8477f7a2bedf43f75
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-01-10 20:27:14 -0500

    task 01/10/2022 done

---
+++ src/main/java/JavaTask01102022.java
@@ -0,0 +1,15 @@
+public class JavaTask01102022 {
+
+    //https://www.codewars.com/kata/56f173a35b91399a05000cb7/train/java
+
+    public static int findLongest(final String str) {
+    String[] spl = str.split("\\s");
+    int longest = 0;
+        for (String s : spl) {
+            if (s.length() > longest) {
+                longest = s.length();
+            }
+        }
+        return longest;
+    }
+}
