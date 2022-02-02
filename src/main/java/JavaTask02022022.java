60c8652e6629ccd656f5d26203d5a76710c50a96
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-02-02 18:23:41 -0500

    task 02/02/2022 done

---
+++ src/main/java/JavaTask02022022.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class JavaTask02022022 {
+
+    //https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/java
+
+    public static String[] kataExampleTwist(){
+
+        String[] websites = new String[1000];
+        String codewars = "codewars";
+        Arrays.fill(websites, codewars);
+
+        return websites;
+    }
+}
