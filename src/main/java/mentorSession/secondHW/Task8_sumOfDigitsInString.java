7994c673ca793d873d4307d4ba3c5556f55f6ee8
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-05-16 16:24:11 -0400

    mentor session HW 2

---
+++ src/main/java/mentorSession/secondHW/Task8_sumOfDigitsInString.java
@@ -0,0 +1,31 @@
+package mentorSession.secondHW;
+
+public class Task8_sumOfDigitsInString {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumOfDigits("123fgfg@@@456"));
+
+    }
+
+    public static int sumOfDigits(String str){
+
+        int sum = 0;
+
+        if(!str.isEmpty() && !str.isBlank()){
+            for(int i = 0; i < str.length(); i++){
+
+                if(Character.isDigit(str.charAt(i))){
+                    sum+=Integer.parseInt(str.charAt(i) + "");
+                }
+            }
+        }
+
+        return sum;
+    }
+}
+
+/*
+8) String - sum of digits in a string
+Write a method that can return the sum of the digits in a string
+ */
