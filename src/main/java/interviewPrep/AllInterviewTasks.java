9c8f605573f94767345fa61b9e42f6e1e245dcfb
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-06-28 21:48:55 -0400

    random

--- src/main/java/interviewPrep/AllInterviewTasks.java
+++ src/main/java/interviewPrep/AllInterviewTasks.java
@@ -6,7 +6,7 @@ public class AllInterviewTasks {
 
 
 
-    /*
+    /*84
  * Given 2 arrays. They are consist of non empty strings. Write a function to find out all elements appear in first array but not in second array.
   Sample input:
     Array A: ["one", "two", "four", "ten"]
---
+++ src/main/java/interviewPrep/All_Random.java
@@ -0,0 +1,12 @@
+package interviewPrep;
+
+import java.util.Random;
+
+public class All_Random {
+
+    public static void main(String[] args) {
+
+        Random rnd = new Random();
+        System.out.println(rnd.nextInt(84));
+    }
+}
