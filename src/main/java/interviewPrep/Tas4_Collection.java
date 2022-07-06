6adebc3a044e49788accb943231ede1c7196dc4d
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-07-05 22:32:24 -0400

    java

---
+++ src/main/java/interviewPrep/Tas4_Collection.java
@@ -0,0 +1,34 @@
+package interviewPrep;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Tas4_Collection {
+
+    public static void main(String[] args) {
+        int a [] = {2,5,7,15,1,6};
+        System.out.println(minMax(a));
+    }
+
+    public static int minMax(int [] arr){
+
+        List<Integer> list = new ArrayList<>();
+
+        for(int each: arr){
+            list.add(each);
+        }
+
+        Collections.sort(list);
+
+        return list.get(list.size() - 1) - list.get(1);
+    }
+}
+
+/*
+4.
+ Use Collection int a [] = {2,5,7,15,1,6};
+   int a [] = {2,5,7,15,1,6};
+   output
+   13   < Max number minus second min num and result is 13
+ */
---
+++ src/main/java/interviewPrep/Task64MultidimensionalArray.java
@@ -0,0 +1,27 @@
+package interviewPrep;
+
+import java.util.Arrays;
+
+public class Task64MultidimensionalArray {
+
+    public static void main(String[] args) {
+        int[][] arr = {{9, 8, 7}, {6}, {5, 4, 3, 2, 1, 0}};
+
+        sortMultidimensionalArr(arr);
+    }
+    /*
+     {  {9, 8, 7}, {6},  {5,4,3,2,1,0}   };    output should be
+0 1 2 3 4 5 6 7 8 9
+     */
+    //int[][] arr = {  {9, 8, 7}, {6},  {5,4,3,2,1,0}   };
+
+    public static void sortMultidimensionalArr(int[][] arr) {
+
+        for (int i = arr.length - 1; i >= 0; i--) { //how many elements in 2D
+            for (int j = 0; j < arr[i].length; j++) { //going through each arr
+                Arrays.sort(arr[i]);
+                System.out.print(arr[i][j]);
+            }
+        }
+    }
+}
