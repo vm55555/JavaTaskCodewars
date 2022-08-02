25e26c0f4b6e002650f32ece8124933164fef140
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-08-02 18:28:14 -0400

    practice

---
+++ src/main/java/interviewPrep/String_RemoveExtraSpace.java
@@ -0,0 +1,27 @@
+package interviewPrep;
+
+public class String_RemoveExtraSpace {
+
+    public static void main(String[] args) {
+        String str = "    I    love     java    very much!  ";
+        System.out.println(removeExtraSpaces(str));
+    }
+
+    public static String removeExtraSpaces(String str){
+
+        str = str.trim();
+        String res = "";
+
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) != ' ' || str.charAt(i + 1) != ' '){
+                res += str.charAt(i);
+            }
+        }
+
+        return res;
+    }
+}
+
+/*
+ String str = "    I    love     java    very much!  ";
+ */
