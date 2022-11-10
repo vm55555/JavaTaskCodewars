7c6a547a9ae97036efbefcedd3961372f8d1cba1
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-11-10 09:30:22 -0500

    practice

---
+++ src/main/java/codingBat/Task11102022.java
@@ -0,0 +1,16 @@
+package codingBat;
+
+public class Task11102022 {
+
+    public static void main(String[] args) {
+        String tag = "i";
+        String word = "Hello";
+    }
+
+    public String makeTags(String tag, String word) {
+        return "<" + tag + ">" + word + "<" + tag + "/>";
+
+    }
+}
+
+//https://codingbat.com/prob/p147483
