2bb28be4dc9fc63634b9184d2cdffc15cdc0b390
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-11-15 09:46:50 -0500

    practice

---
+++ src/main/java/codingBat/Task11152022.java
@@ -0,0 +1,20 @@
+package codingBat;
+
+public class Task11152022 {
+
+    public static void main(String[] args) {
+        String str = "Hello";
+    }
+
+    public String firstTwo(String str) {
+        if(str.length() > 2){
+
+            return str.substring(0,2);
+        }
+
+        return str;
+    }
+
+}
+
+//https://codingbat.com/prob/p163411
