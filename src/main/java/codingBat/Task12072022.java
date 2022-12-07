b20126449741ce0dc7bf8d4d0345227448b9e78f
Viktoriia Mishchenko <viktoriiamish@gmail.com>
2022-12-07 10:19:58 -0500

    practice

---
+++ src/main/java/codingBat/Task12072022.java
@@ -0,0 +1,12 @@
+package codingBat;
+
+public class Task12072022 {
+
+    public String middleTwo(String str) {
+
+       return "" + str.charAt(str.length()/2 - 1) + str.charAt(str.length()/2);
+
+    }
+}
+
+//https://codingbat.com/prob/p137729
